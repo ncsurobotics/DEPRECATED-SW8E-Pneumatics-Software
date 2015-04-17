8dacb3dd2030cdef97c5dfa92824a5825e348961
dralley <dralley@ncsu.edu>
2015-04-17 12:40:47 -0400

    formatted, changed string-states to constants

--- pneumatics.py
+++ pneumatics.py
@@ -1,155 +1,176 @@
 import sys
 sys.path.insert(0, '../acoustics/Apps/pinger_finder/')
 
-import boot #python script for muxing pin configs
+import boot  # python script for muxing pin configs
 import BBBIO
 
-#Pins for pneumatics
+
+class State(object):
+    """ State constants """
+    OPEN = 0
+    CLOSED = 1
+
+# Pins for pneumatics
 PEDO_PIN = 'P8_07'
 GRABBER_PIN = 'P8_08'
-DEFAULT_GRABBER_STATE = "closed"
+DEFAULT_GRABBER_STATE = State.CLOSED
 
-#Strings for printing
+# Strings for printing
 QUERY_HDL = ">> "
 
+
 def main():
-	#Display init sequence
-	init()
+    # Display init sequence
+    init()
 
-	#Bring up user interface
-	ui()
+    # Bring up user interface
+    ui()
 
-	#exit
-	exit_app()
+    # exit
+    exit_app()
+
 
 def init():
-	print('-'*50)
-	print("-- Pneumatics App --")
-	print('-'*50)
+    print('-' * 50)
+    print("-- Pneumatics App --")
+    print('-' * 50)
+
 
 def exit_app():
-	print("Goodbye!!!")
+    print("Goodbye!!!")
+
 
 def ui():
-	# Load the pneumatics
-	PN = pneumatics()
-	
-	# Greet the user
-	print("Welcome! Please enter a command below:")
-	
-	q = 0
-	while(q == 0):
-		#query user for input	
-		user_input = query(QUERY_HDL)
-		
-		# c ############################
-		########### user Class ##
-		################################
-		if ('quit' == user_input) or ('q' == user_input):
-			q = 1
-			PN.close()
+    # Load the pneumatics
+    PN = Pneumatics()
 
-		elif ('help' == user_input) or ('h' == user_input):
-			print('-'*50)
-			usage()
-			print('-'*50)
+    # Greet the user
+    print("Welcome! Please enter a command below:")
 
-		# c ############################
-		########### torpedo class ##
-		################################
-		elif ('fire' == user_input) or ('f' == user_input):
-			print("Firing torpedo!")
-			PN.fireTorp()
+    q = 0
+    while(q == 0):
+    # query user for input
+        user_input = query(QUERY_HDL)
 
-		elif('grabber' == user_input) or ('g' == user_input):
-			if PN.grabber_status=="open":
-				print("Closing Grabber")
-				PN.closeGrabber()
-			elif PN.grabber_status=="closed":
-				print("Opening Grabber")
-				PN.openGrabber()
-			else:
-				print("Hmm... grabber has no status.")
+        # c ############################
+        ########### user Class ##
+        ################################
+        if ('quit' == user_input) or ('q' == user_input):
+            q = 1
+            PN.close()
 
-		# c ############################
-		########### utility cmd Class ##
-		################################
-		elif 'EOF' == user_input:
-			response(loc.curr, "End of input file reached")
+        elif ('help' == user_input) or ('h' == user_input):
+            print('-' * 50)
+            usage()
+            print('-' * 50)
 
-		else:
-			print("Not a recognized command! Try again.")
+        # c ############################
+        ########### torpedo class ##
+        ################################
+        elif ('fire' == user_input) or ('f' == user_input):
+            print("Firing torpedo!")
+            PN.fireTorp()
+
+        elif('grabber' == user_input) or ('g' == user_input):
+            if PN.grabber_status == State.OPEN:
+                print("Closing Grabber")
+                PN.closeGrabber()
+            elif PN.grabber_status == State.CLOSED:
+                print("Opening Grabber")
+                PN.openGrabber()
+            else:
+                print("Hmm... grabber has no status.")
+
+        # c ############################
+        ########### utility cmd Class ##
+        ################################
+        elif 'EOF' == user_input:
+            response(loc.curr, "End of input file reached")
+
+        else:
+            print("Not a recognized command! Try again.")
 
 
 def query(hdl):
-	return raw_input(hdl)
+    return raw_input(hdl)
+
+
 def usage():
-	text = ["  (h)elp: Get help.\n",
-		"  (q)uit: quit this program.\n",
-		"  (s)tatus: prints data regarding pneumatics.\n",
-		#"  c(o)nfig: Change Settings of the pneumatics system.\n",
-		"  (f)ire: fire the torpedos.\n",
-		"  (g)rabber: toggle the grabber state.\n"]
-	print('\n'.join(text) )
+    text = ["  (h)elp: Get help.\n",
+            "  (q)uit: quit this program.\n",
+            "  (s)tatus: prints data regarding pneumatics.\n",
+            #"  c(o)nfig: Change Settings of the pneumatics system.\n",
+            "  (f)ire: fire the torpedos.\n",
+            "  (g)rabber: toggle the grabber state.\n"]
+    print('\n'.join(text))
 
-class pneumatics:
-	def __init__(self):
-		#Instantiate pedo pin
-		self.pedo = BBBIO.Port(PEDO_PIN)
-		self.pedo.setPortDir("out")
 
-		#instantiate grabber pin
-		self.grabber = BBBIO.Port(GRABBER_PIN)
-		self.grabber.setPortDir("out")
-		self.grabber_status = DEFAULT_GRABBER_STATE
+class Pneumatics:
 
-	def reinit(self):
-		self.__init__()
+    def __init__(self):
+        # Instantiate pedo pin
+        self.pedo = BBBIO.Port(PEDO_PIN)
+        self.pedo.setPortDir("out")
 
-	def close(self):
-		self.pedo.close()
-		self.grabber.close()
+        # instantiate grabber pin
+        self.grabber = BBBIO.Port(GRABBER_PIN)
+        self.grabber.setPortDir("out")
+        self.grabber_status = DEFAULT_GRABBER_STATE
 
-	def help(self):
-		"""help: prints a list of all available attribute and methods
-		for (self)."""
+    def reinit(self):
+        """
+        """
+        self.__init__()
 
-		print("You can call the following attributes: ")
-		for item in self.__dict__.keys():
-			print("  *.%s" % item)
-		
-		print("")
-		print("You also have access to the following methods: ")
-		for item in [method for method in dir(self) if callable(getattr(self, method))]:
-			print("  *.%s()" % item)
-		print("")
+    def close(self):
+        """
+        """
+        self.pedo.close()
+        self.grabber.close()
 
-	#######################
-	## Universal Methods ##
-	#######################
-	#method for reseting the config
-	def reset(self):
-		self.pedo.writeToPort(0)
-		self.grabber.writeToPort(0)
+    def help(self):
+        """ Prints a list of all available attribute and methods for (self)
+        """
 
-	######################
-	## Torpedo Methods ##
-	######################
-	#method for firing the torpedo
-	def fireTorp(self):
-		self.pedo.writeToPort(1)
-		self.pedo.writeToPort(0)
+        print("You can call the following attributes: ")
+        for item in self.__dict__.keys():
+            print("  *.%s" % item)
 
-	#####################
-	## Grabber methods ##
-	#####################
-	def closeGrabber(self):
-		self.grabber.writeToPort(1)
-		self.grabber_status = "closed"
+        print("")
+        print("You also have access to the following methods: ")
+        for item in [method for method in dir(self) if callable(getattr(self, method))]:
+            print("  *.%s()" % item)
+        print("")
 
-	def openGrabber(self):
-		self.grabber.writeToPort(0)
-		self.grabber_status = "open"
+    #######################
+    ## Universal Methods ##
+    #######################
+    def reset(self):
+        """ Reset the config
+        """
+        self.pedo.writeToPort(0)
+        self.grabber.writeToPort(0)
 
-		
-	
+    ######################
+    ## Torpedo Methods ##
+    ######################
+    def fireTorp(self):
+        """ Fire the torpedo
+        """
+        self.pedo.writeToPort(1)
+        self.pedo.writeToPort(0)
+
+    #####################
+    ## Grabber methods ##
+    #####################
+    def closeGrabber(self):
+        """ Close the grabber
+        """
+        self.grabber.writeToPort(1)
+        self.grabber_status = State.CLOSED
+
+    def openGrabber(self):
+        """ Open the grabber
+        """
+        self.grabber.writeToPort(0)
+        self.grabber_status = State.OPEN
